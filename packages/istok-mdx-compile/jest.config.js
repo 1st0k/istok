8bad5380d72db8d5d7a1ff9d92e4d9f753340a01
Alexander Snov <snov.contact@gmail.com>
2021-05-30 18:59:47 +0700

    Fix tests

--- packages/istok-mdx-compile/jest.config.js
+++ packages/istok-mdx-compile/jest.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
 
-const babelJestConfig = { configFile: path.resolve(__dirname, './.babelrc.test') };
+const babelJestConfig = { configFile: path.resolve(__dirname, './.babelrc') };
+
+const esModules = ['unist-util-'].join('|');
 
 module.exports = {
   testEnvironment: 'jsdom',
+  moduleDirectories: ['node_modules', '<rootDir>/src'],
   transform: {
-    '.(ts|tsx)$': require.resolve('ts-jest/dist'),
+    '.(js|jsx|ts|tsx)$': [require.resolve('babel-jest'), babelJestConfig], //require.resolve('ts-jest/dist'),
     '.(js|jsx)$': [require.resolve('babel-jest'), babelJestConfig],
   },
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`, '\\.pnp\\.[^\\/]+$'],
 };
