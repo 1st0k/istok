9a132a971d854f11d8e909bdae1a6d83e5b759c6
Alexander Snov <snov.contact@gmail.com>
2021-06-12 22:42:17 +0700

    Export Source utilities

--- packages/istok-core/src/index.ts
+++ packages/istok-core/src/index.ts
@@ -1,6 +1,6 @@
 export { Result, ResultError, ResultKind, ResultSuccess, error, success, successExt } from './Result';
 export { Entity, EntityRespone, Id, Identifiable, entityResponse } from './Entity';
-export { Source } from './Source';
+export { Source, QueryFilter, QueryParams, QueryPartialResult, QueryResult } from './Source';
 export { MemorySourceOptions, createMemorySource } from './MemorySource';
 export { createSourceSequence, SourceSequence, CreateSourceSequenceOptions } from './SourceSequence';
 export { CachableSource, CachableSourceOptions, CacheLevelOptions, createCachableSource } from './CachableSource';
