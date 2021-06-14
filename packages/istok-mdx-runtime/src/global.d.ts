a19b0c6f026c7828d173fda704a7d2c1eb1f2806
Alexander Snov <snov.contact@gmail.com>
2021-06-14 16:41:45 +0700

    Upgrade @mdx-js

--- packages/istok-mdx-runtime/src/global.d.ts
+++ packages/istok-mdx-runtime/src/global.d.ts
@@ -1,9 +1,3 @@
-declare module '@mdx-js/react' {
-  const Mdx: any;
-  export const mdx: any;
-  export const MDXProvider: any;
-}
-
 declare interface Window {
   requestIdleCallback(cb: Function): NodeJS.Timeout;
   cancelIdleCallback(id: NodeJS.Timeout): void;
